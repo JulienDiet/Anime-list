7ee05beb6b88790f63dc724fc04adafc1cf280f3
JulienDiet <juliendiet7@gmail.com>
2024-06-16 15:45:57 +0200

    db gestion move into a dedicated folder

---
+++ database/script_db.py
@@ -0,0 +1,7 @@
+import utile.fetch_api as fetch_api
+import utile.data as data
+
+
+def fetch_and_store_top_50_animes():
+    top_50_animes = fetch_api.top_50_animes_from_myanimelist()
+    data.store_animes_in_db(top_50_animes)
--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import utile.fetch_api as fetch_api
-import utile.data as data
-
-
 def main():
-    top_50_animes = fetch_api.top_50_animes_from_myanimelist()
-    print(top_50_animes)
-    data.store_animes_in_db(top_50_animes)
+    pass
 
 
 if __name__ == '__main__':
